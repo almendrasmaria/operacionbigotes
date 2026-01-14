0a7e1f703590f37c8b9fe2409a95db1369b353c7
maria <almendrasmaria04@gmail.com>
2026-01-14 16:43:32 -0300

    include GuidesPage in router

--- frontend/src/routes/Rutas.jsx
+++ frontend/src/routes/Rutas.jsx
@@ -1,9 +1,11 @@
 import { useRoutes } from "react-router-dom";
 import Home from "../home/Page";
+import GuidesPage from "../guides/Page";
 
 const Rutas = () => {
   const element = useRoutes([
     { path: "/", element: <Home /> },
+    { path: "/guias", element: <GuidesPage /> },
   ]);
 
   return element;
